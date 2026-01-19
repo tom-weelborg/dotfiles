9a8533b478f26b8dd9037aafe68edc45af37707f
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-01-19 18:23:18 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem{
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
