70c9658d5ab08aadcfe2c3e42e803dccbb6ffcf9
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-01-20 13:11:15 +0100

    Replace vscodium extension channels with vscode-extensions

--- modules/vscodium/default.nix
+++ modules/vscodium/default.nix
@@ -3,12 +3,7 @@
   environment.systemPackages = [
     (pkgs.vscode-with-extensions.override {
       vscode = pkgs.vscodium;
-      vscodeExtensions =
-        with pkgs.vscode-marketplace;
-        with pkgs.vscode-marketplace-release;
-        with pkgs.open-vsx;
-        with pkgs.open-vsx-release;
-      [
+      vscodeExtensions = with pkgs.vscode-extensions; [
       ];
     })
   ];
