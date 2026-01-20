114ab2dfb27836fb4713afd2ed4cce64512c351f
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-01-20 11:41:00 +0100

    Replace grub with efi

--- configuration.nix
+++ configuration.nix
@@ -11,9 +11,8 @@
     ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   
   nix.settings.experimental-features = [
     "nix-command"
