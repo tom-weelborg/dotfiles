f542be5b315df1c48ced6200b0bf4976f85068b2
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-01-21 17:13:48 +0100

    Make firefox default browser

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,14 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  xdg.mime.defaultApplications = {
+    "text/html" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+    "x-scheme-handler/about" = "firefox.desktop";
+    "x-scheme-handler/unknown" = "firefox.desktop";
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
