4a28f7d2a475f140bf2e8bdecb8d0381c8109638
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-01-21 13:02:02 +0100

    Update desktopManager integration in configuration

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
