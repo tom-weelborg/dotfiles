dfe0f098c995e5bec01bde23c89a7bab0836516e
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-02-22 19:54:42 +0100

    Allow extension profiles in vscodium module

--- modules/programs/gui/development/vscodium/default.nix
+++ modules/programs/gui/development/vscodium/default.nix
@@ -1,25 +1,43 @@
-{ pkgs, variables, ... }:
 {
-  environment.systemPackages = [
-    (pkgs.vscode-with-extensions.override {
-      vscode = pkgs.vscodium;
-      vscodeExtensions =
-        with pkgs.vscode-marketplace;
-        with pkgs.vscode-marketplace-release;
-        with pkgs.open-vsx;
-        with pkgs.open-vsx-release;
-        with pkgs.vscode-extensions;
-      [
-        # Nix
-        jnoortheen.nix-ide
-      ];
-    })
-  ];
-
-  home-manager.users.${variables.username} = { config, ... }: {
-    xdg.configFile."VSCodium/User/settings.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink (./settings.json);
-      force = true;
+  options = lib:
+    {
+      extensionProfiles = {
+        nix = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+        };
+      };
     };
-  };
+
+  module = { moduleConfig, pkgs, variables, ... }:
+    let
+      getExtensions = extensionProfiles:
+        let
+          extensionProfilesToInclude = builtins.filter
+            (k: extensionProfiles.${k})
+            (builtins.attrNames extensionProfiles);
+
+          result = builtins.concatLists (
+            builtins.map
+              (k: import ./extensionProfiles/${k}.nix { inherit pkgs; })
+              extensionProfilesToInclude
+          );
+        in
+          result;
+    in
+    {
+      environment.systemPackages = [
+        (pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
+          vscodeExtensions = getExtensions moduleConfig.extensionProfiles;
+        })
+      ];
+
+      home-manager.users.${variables.username} = { config, ... }: {
+        xdg.configFile."VSCodium/User/settings.json" = {
+          source = config.lib.file.mkOutOfStoreSymlink (./settings.json);
+          force = true;
+        };
+      };
+    };
 }
