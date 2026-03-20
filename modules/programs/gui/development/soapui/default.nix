beb3e50bd057b4e893fed983784a9fbebfdbb2c7
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-03-20 18:54:24 +0100

    Add desktop entry for SoapUI

--- modules/programs/gui/development/soapui/default.nix
+++ modules/programs/gui/development/soapui/default.nix
@@ -1,6 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, variables, ... }:
 {
   environment.systemPackages = [
     pkgs.soapui
   ];
+
+  home-manager.users.${variables.username} = {
+    xdg.desktopEntries.soapui = {
+        name = "SoapUI";
+        genericName = "API Testing Tool";
+        exec = "soapui";
+        icon = "soapui";
+        terminal = false;
+        categories = ["Development" "Network"];
+    };
+  };
 }
