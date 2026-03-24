c8e6f6e971c1304e7b122fb116529467ef19b82c
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-03-24 18:49:45 +0100

    Add dotnet extension profile to vscodium

---
+++ modules/programs/gui/development/vscodium/extensionProfiles/dotnet.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+with pkgs.vscode-marketplace;
+with pkgs.vscode-marketplace-release;
+with pkgs.open-vsx;
+with pkgs.open-vsx-release;
+with pkgs.vscode-extensions;
+[
+  ms-dotnettools.csdevkit
+  ms-dotnettools.csharp
+  ms-dotnettools.vscode-dotnet-runtime
+]
