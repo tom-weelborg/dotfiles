824bb42210019991ebee68443b0d4a3340d9ea33
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-06-10 22:56:11 +0200

    Update nixpkgs and home-manager to 26.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     home-manager = {
-      url = "github:nix-community/home-manager?ref=release-25.11";
+      url = "github:nix-community/home-manager?ref=release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-vscode-extensions = {
