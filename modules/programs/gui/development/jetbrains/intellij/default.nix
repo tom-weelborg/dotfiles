809ae62a6b4682d7d6450dba2859978c0481e892
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-06-10 14:18:55 +0200

    Add vmoptions to Intellij

--- modules/programs/gui/development/jetbrains/intellij/default.nix
+++ modules/programs/gui/development/jetbrains/intellij/default.nix
@@ -1,6 +1,32 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs.jetbrains; [
-    idea
-  ];
+  options = lib:
+    {
+      vmoptions = lib.mkOption {
+        type = lib.types.str;
+        default = "-Xmx4096m";
+      };
+    };
+
+  module = { moduleConfig, pkgs, variables, ... }:
+    {
+      environment.systemPackages = with pkgs.jetbrains; [
+        idea
+      ];
+
+      home-manager.users.${variables.username} = { lib, ... }:
+        {
+          home.activation.intellijVmOptions =
+            lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+              jetbrains_dir="$HOME/.config/JetBrains"
+
+              if [ -d "$jetbrains_dir" ]; then
+                for dir in "$jetbrains_dir"/IntelliJIdea*; do
+                  if [ -d "$dir" ]; then
+                    echo "${moduleConfig.vmoptions}" > "$dir/idea64.vmoptions"
+                  fi
+                done
+              fi
+            '';
+        };
+    };
 }
