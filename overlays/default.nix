11a6dac2ac460344496a32bf6f013b850b313d86
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-06-12 17:16:54 +0200

    Reorder overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,10 +2,9 @@
 {
   nixpkgs.overlays = [
     inputs.firefox-addons.outputs.overlays.default
-    
+    inputs.nix-vscode-extensions.overlays.default
+
     (import ./lib.nix)
     (import ./requestly-firefox-addon.nix)
-
-    inputs.nix-vscode-extensions.overlays.default
   ];
 }
