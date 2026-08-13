db8f0aed0ea2787f24c1b652838d30fe4a4a39a5
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-08-14 00:23:23 +0200

    Add NFS server to home-nas-2026-07

--- hosts/home/nas/2026-07/configuration.nix
+++ hosts/home/nas/2026-07/configuration.nix
@@ -4,8 +4,27 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPVKaWEUtl0aXRSDalYuPlJroe+KqQM1BCaUXhpxp/1j nas-login"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK8y2v3JYqacZn6nySkaksP1YYIG+4zk3NuwaK2+faJs nas-login"
   ];
+
+  jellyfinGid = 1000;
 in
 {
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /export 192.168.178.0/24(rw,fsid=0,no_subtree_check,all_squash,anonuid=993,anongid=${toString jellyfinGid})
+    '';
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    2049
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    2049
+  ];
+
+  users.groups.jellyfin.gid = jellyfinGid;
+
   systemd = {
     network = {
       enable = true;
