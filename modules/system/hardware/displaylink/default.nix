3a626a0f847c98fe1cbcb85f79ba2486582e266d
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-09-14 18:27:17 +0200

    Update EVDI to version 1.15.0

--- modules/system/hardware/displaylink/default.nix
+++ modules/system/hardware/displaylink/default.nix
@@ -20,6 +20,38 @@
         systemd.services.dlm.wantedBy = [
           "multi-user.target"
         ];
+
+        nixpkgs.overlays = [
+          (final: prev: {
+            linuxPackages_latest =
+              prev.linuxPackages_latest.extend
+                (lpfinal: lpprev: {
+                  evdi = lpprev.evdi.overrideAttrs (efinal: eprev: rec {
+                    version = "1.15.0";
+                    src = final.fetchFromGitHub {
+                      owner = "DisplayLink";
+                      repo = "evdi";
+                      rev = "v${version}";
+                      sha256 = "sha256-CXF7PvmrPjjNoWXbWxEkFE/Sw4bO6YqDplPwF/OxhB0=";
+                    };
+                    prePatch = "";
+                    postPatch = "";
+                    patches = [
+                      ./fix-conftest-probes.patch
+                    ];
+                    env = (eprev.env or { }) // {
+                      CFLAGS = toString [
+                        "-Wno-error"
+                        "-Wno-error=sign-compare"
+                      ];
+                    };
+                  });
+                });
+            displaylink = prev.displaylink.override {
+              inherit (final.linuxPackages_latest) evdi;
+            };
+          })
+        ];
       };
     };
 }
